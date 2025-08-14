6d3035278896c682c875e8d939ac10de4cf79102
Flapchik <58729268+Flapchik@users.noreply.github.com>
2025-08-14 18:45:56 +0300

    git init

---
+++ activity_cog.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+
+class ActivityCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='Roblox'))
+
+
+def setup(bot):
+    bot.add_cog(ActivityCog(bot))
